f9de4d11fdc416eb72d8e16bdf9938e17fddc633
Vivek Shenoy <vivekshenoy2000@gmail.com>
2022-05-20 12:09:44 +0530

    Create-React_App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">MyPharma</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
